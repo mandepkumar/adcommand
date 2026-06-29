e912f9c0e4e84f7ffed0aa27342e94e7b74a74f7
mandepkumar <162789420+mandepkumar@users.noreply.github.com>
2026-06-29 07:54:20 +0530

    #1 d1

--- dev/prebid_pre.js
+++ dev/prebid_pre.js
@@ -1,141 +1,248 @@
-// Size Mapping
-var adSizes = [];
-adSizes["RIghtSidebar_Bottom"] = [[]];       // 160x600, 300x600, 300x250 dt
+const jsdoc = require('eslint-plugin-jsdoc')
+const lintImports = require('eslint-plugin-import')
+const neostandard = require('neostandard')
+const babelParser = require('@babel/eslint-parser');
+const globals = require('globals');
+const prebid = require('./plugins/eslint/index.js');
+const {includeIgnoreFile} = require('@eslint/compat');
+const path = require('path');
+const _ = require('lodash');
 
-export const tcAdunits = [{
-    code: 'Top',
-    device: [''],
-    mediaTypes: { banner: { sizes: [[300,250]] } },
-    bids: [
-        { bidder: "appnexus", params: { placementId: "20816030" } },
-        { bidder: "openx", params: { "unit": "561042203", "delDomain": "ascendeum-d.openx.net" } },
-        //{ bidder: "seedtag", params: { "adUnitId": "33718445", "publisherId": "9247-2848-01", "placement": "inBanner" } },
-        { bidder: "rubicon", params: { accountId: '22936', siteId: '359482', zoneId: '1936110' } },
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Top_300x250" } },
-        { bidder: "ix", params: {  siteId: "620102" } },
-        { bidder: "triplelift", params: { inventoryCode: "TimeCardCalculator_Mobile_Top" } },
-        { bidder: "sharethrough", params: { pkey: "oyFpL7dIonSq36U4tewLJQqA" } },
-        { bidder: "yieldmo", params: { placementId: '2834829907232497683' } },
-        { bidder: "nextMillennium", params: { placement_id: '25717' } }, // mobile
-        { bidder: "smartadserver", params: { siteId: 518853, pageId: 1623524, formatId: 113720 } },
-        { bidder: "rise", params: { org: '63889b5a59cc190001431f40' } },
-        { bidder: "unruly", params: { siteId: 276828 }},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'siab' }},
-        { bidder: "improvedigital", params :{ publisherId: 2445, placementId: 23260219}},
-        { bidder: "minutemedia", params: { org: '01j1800kyzkr', placementId: "TimeCardCalculator_Top_300x250" } },
-        { bidder: "vidazoo", params: { cId: "6683f48c24368641f0ed0dc9", pId: "59ac17c192832d0011283fe3" ,subDomain: "exchange" } }
-    ]
-}, {
-    code: 'RightSidebar_Top',
-    device: ['Desktop', 'Tablet'],
-    mediaTypes: { banner: { sizes: [[300,600]] } },
-    bids: [
-        { bidder: "appnexus", params: { placementId: "20816031" } },
-        //{ bidder: "seedtag", params: { "adUnitId": "33718446", "publisherId": "9247-2848-01", "placement": "inBanner" } },
-        { bidder: "openx", params: { "unit": "561042204", "delDomain": "ascendeum-d.openx.net" } },
-        { bidder: "triplelift", params: { inventoryCode: "TimeCardCalculator_RIghtSidebar_Top" } },
-        { bidder: 'rubicon', params: { accountId: '22936', siteId: '359482', zoneId: '1936114' } },
-        { bidder: "ix", params: {  siteId: "620101" } },
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_RIghtSidebar_Top_300x600" } },
-        { bidder: "sharethrough", params: { pkey: "oyFpL7dIonSq36U4tewLJQqA" }},
-        { bidder: "yieldmo", params: { placementId: '2834829907073114130' } },
-        { bidder: "nextMillennium", params: { placement_id: '25715' } },
-        { bidder: "smartadserver", params: { siteId: 518853, pageId: 1623523, formatId: 113722 } },
-        { bidder: "rise", params: { org: '63889b5a59cc190001431f40' } },
-        { bidder: "unruly", params: { siteId: 276828 }},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'siab' }},
-        { bidder: "improvedigital", params :{ publisherId: 2445, placementId: 23260217}},
-        { bidder: "minutemedia", params: { org: '01j1800kyzkr', placementId: "TimeCardCalculator_RIghtSidebar_Top" } },
-        { bidder: "vidazoo", params: { cId: "6683f48c24368641f0ed0dc9", pId: "59ac17c192832d0011283fe3" ,subDomain: "exchange" } }
-    ]
-}, {
-    code: 'RightSidebar_Bottom',
-    device: ['Desktop', 'Tablet'],
-    mediaTypes: { banner: { sizes: adSizes['RIghtSidebar_Bottom'] } },
-    bids: []
-}, {
-    code: 'Bottom',
-    device: [''],
-    mediaTypes: { banner: { sizes: [[300,250]] } },
-    bids: [
-        { bidder: "appnexus", params: { placementId: "20816033" } },
-        //", params: { "adUnitId": "33718449", "publisherId": "9247-2848-01", "placement": "inBanner" } },
-        { bidder: "openx", params: { "unit": "561042207", "delDomain": "ascendeum-d.openx.net" } },
-        { bidder: 'rubicon', params: { accountId: '22936', siteId: '359482', zoneId: '1936118' } },
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Bottom_300x250" } },
-        { bidder: "ix", params: {  siteId: "620099" } },
-        { bidder: "triplelift", params: { inventoryCode: "TimeCardCalculator_Mobile_Bottom" } },
-        { bidder: "sharethrough", params: { pkey: "oyFpL7dIonSq36U4tewLJQqA" }},
-        { bidder: "yieldmo", params: { placementId: '2834829906745958416' } },
-        // { bidder: "nextMillennium", params: { placement_id: '25712' } }, // desktop
-        { bidder: "nextMillennium", params: { placement_id: '25713' } }, // mobile 
-        { bidder: "smartadserver", params: { siteId: 518853, pageId: 1623521, formatId: 113721 } },
-        { bidder: "rise", params: { org: '63889b5a59cc190001431f40' } },
-        { bidder: "unruly", params: { siteId: 276828 }},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'siab' }},
-        { bidder: "improvedigital", params :{ publisherId: 2445, placementId: 23260213}},
-        { bidder: "minutemedia", params: { org: '01j1800kyzkr', placementId: "TimeCardCalculator_Bottom_300x250" } },
-        { bidder: "vidazoo", params: { cId: "6683f48c24368641f0ed0dc9", pId: "59ac17c192832d0011283fe3" ,subDomain: "exchange" } }
-    ]
-}, {
-    code: 'Mobile_Adhesion',
-    device: [''],
-    mediaTypes: { banner: { sizes: [[320,50]] } },
-    bids: [
-        { bidder: "appnexus", params: { placementId: "21047292" } },
-        //{ bidder: "seedtag", params: { "adUnitId": "33718444", "publisherId": "9247-2848-01", "placement": "inScreen" } },
-        { bidder: "openx", params: { "unit": "561042209", "delDomain": "ascendeum-d.openx.net" } },
-        { bidder: "ix", params: {  siteId: "631289" } },
-        { bidder: "triplelift", params: { inventoryCode: "TimeCardCalculator_Mobile_Adhesion_320x50" } },
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Mobile_Adhesion" } },
-        { bidder: 'rubicon', params: { accountId: '22936', siteId: '359482', zoneId: '1975906' } },
-        { bidder: "sharethrough", params: { pkey: "oyFpL7dIonSq36U4tewLJQqA" }},
-        { bidder: "rise", params: { org: '63889b5a59cc190001431f40' } },
-        { bidder: "unruly", params: { siteId: 276828 }},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'inview' }},
-        { bidder: "minutemedia", params: { org: '01j1800kyzkr', placementId: "TimeCardCalculator_Mobile_Adhesion" } },
-        { bidder: "vidazoo", params: { cId: "6683f48c24368641f0ed0dc9", pId: "59ac17c192832d0011283fe3" ,subDomain: "exchange" } }
-    ]
-},{
-    code:'Right1',    
-    device: ['Desktop','Tablet'],
-    mediaTypes:{ banner:{sizes: [[300,250],[300,50],[320,50],[320,100],[250,250],[120,240],[1,1]]}},
-    bids:[
-        { bidder: "rubicon", params: { accountId: '22936', siteId: '359482', zoneId: '3496116' } },
-        { bidder: "ix", params: { siteId: "1127263"}},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right1_250x250" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right1_300x250" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right1_300x50" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right1_320x100" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right1_320x50" }},
-        { bidder: "openx", params: { "unit": "561042210", "delDomain": "ascendeum-d.openx.net" } },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'siab' }},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} }
-        //{ bidder: "seedtag", params: { "adUnitId": "33737028", "publisherId": "9247-2848-01", "placement": "inBanner" } }
-    ]
-},{
-    code:'Right2',  
-    isPrefix:true,
-    device: ['Desktop','Tablet'],
-    mediaTypes:{ banner:{sizes: [[300,250],[300,50],[320,50],[320,100],[250,250],[120,240],[1,1]]}},
-    bids:[
-        { bidder: "rubicon", params: { accountId: '22936', siteId: '359482', zoneId: '3496118' } },
-        { bidder: "ix", params: { siteId: "1127262"}},
-        { bidder: 'pulsepoint', params: { cp: 563495, ct: 773638} },
-        { bidder: '33across', params: { siteId: 'cMHD_gyo8r74o-rkHcnlxd', productId: 'siab' }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right2_250x250" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right2_300x250" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right2_300x50" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right2_320x100" }},
-        { bidder: "pubmatic", params: { publisherId: "160323", adSlot: "TimeCardCalculator_Right2_320x50" }},
-        { bidder: "openx", params: { "unit": "561042211", "delDomain": "ascendeum-d.openx.net" } }
-        //{ bidder: "seedtag", params: { "adUnitId": "33763128", "publisherId": "9247-2848-01", "placement": "inBanner" } }   
-    ]
-}];
+function sourcePattern(name) {
+  return [`${name}/**/*.js`, `${name}/**/*.mjs`]
+}
 
+const sources = ['src', 'modules', 'libraries', 'creative'].flatMap(sourcePattern)
+const autogen = 'libraries/creative-renderer-*/**/*'
 
+const allowedImports = {
+  modules: [
+    'crypto-js',
+    'live-connect' // Maintained by LiveIntent : https://github.com/liveintent-berlin/live-connect/
+  ],
+  src: [
+    'fun-hooks/no-eval',
+    'klona',
+    'dlv',
+    'dset'
+  ],
+  libraries: [],
+  creative: [],
+}
 
+function noGlobals(names) {
+  return {
+    globals: Object.entries(names).map(([name, message]) => ({
+      name,
+      message
+    })),
+    props: Object.entries(names).map(([name, message]) => ({
+      object: 'window',
+      property: name,
+      message
+    }))
+  }
+}
+
+function commonConfig(overrides) {
+  return _.merge({
+    plugins: {
+      jsdoc,
+      import: lintImports,
+      prebid
+    },
+    settings: {
+      jsdoc: {
+        tagNamePreference: {
+          return: 'return'
+        }
+      }
+    },
+    languageOptions: {
+      parser: babelParser,
+      sourceType: 'module',
+      ecmaVersion: 2018,
+      globals: {
+        BROWSERSTACK_USERNAME: false,
+        BROWSERSTACK_KEY: false,
+        FEATURES: 'readonly',
+        ...globals.browser
+      },
+    },
+    rules: {
+      'comma-dangle': 'off',
+      semi: 'off',
+      'space-before-function-paren': 'off',
+      'import/extensions': ['error', 'ignorePackages'],
+
+      // Exceptions below this line are temporary (TM), so that eslint can be added into the CI process.
+      // Violations of these styles should be fixed, and the exceptions removed over time.
+      //
+      // See Issue #1111.
+      // also see: reality. These are here to stay.
+
+      eqeqeq: 'off',
+      'no-return-assign': 'off',
+      'no-throw-literal': 'off',
+      'no-undef': 2,
+      'no-useless-escape': 'off',
+      'no-console': 'error',
+      'jsdoc/check-types': 'off',
+      'jsdoc/no-defaults': 'off',
+      'jsdoc/newline-after-description': 'off',
+      'jsdoc/require-jsdoc': 'off',
+      'jsdoc/require-param': 'off',
+      'jsdoc/require-param-description': 'off',
+      'jsdoc/require-param-name': 'off',
+      'jsdoc/require-param-type': 'off',
+      'jsdoc/require-property': 'off',
+      'jsdoc/require-property-description': 'off',
+      'jsdoc/require-property-name': 'off',
+      'jsdoc/require-property-type': 'off',
+      'jsdoc/require-returns': 'off',
+      'jsdoc/require-returns-check': 'off',
+      'jsdoc/require-returns-description': 'off',
+      'jsdoc/require-returns-type': 'off',
+      'jsdoc/require-yields': 'off',
+      'jsdoc/require-yields-check': 'off',
+      'jsdoc/tag-lines': 'off',
+      'no-var': 'off',
+      'no-empty': 'off',
+      'no-void': 'off',
+      'array-callback-return': 'off',
+      'import-x/no-named-default': 'off',
+      'prefer-const': 'off',
+      'no-prototype-builtins': 'off',
+      'object-shorthand': 'off',
+      'prefer-regex-literals': 'off',
+      'no-case-declarations': 'off',
+      'no-useless-catch': 'off',
+      '@stylistic/quotes': 'off',
+      '@stylistic/quote-props': 'off',
+      '@stylistic/array-bracket-spacing': 'off',
+      '@stylistic/object-curly-spacing': 'off',
+      '@stylistic/semi': 'off',
+      '@stylistic/space-before-function-paren': 'off',
+      '@stylistic/multiline-ternary': 'off',
+      '@stylistic/computed-property-spacing': 'off',
+      '@stylistic/lines-between-class-members': 'off',
+      '@stylistic/indent': 'off',
+      '@stylistic/comma-dangle': 'off',
+      '@stylistic/object-curly-newline': 'off',
+      '@stylistic/object-property-newline': 'off',
+      '@stylistic/no-multiple-empty-lines': 'off',
+
+    }
+  }, overrides);
+}
+
+module.exports = [
+  includeIgnoreFile(path.resolve(__dirname, '.gitignore')),
+  {
+    ignores: [
+      autogen,
+      'integrationExamples/**/*',
+      // do not lint build-related stuff
+      '*.js',
+      ...sourcePattern('plugins'),
+      ...sourcePattern('.github'),
+    ],
+  },
+  jsdoc.configs['flat/recommended'],
+  ...neostandard({
+    files: sources,
+  }),
+  commonConfig({
+    files: sources,
+  }),
+  ...Object.entries(allowedImports).map(([path, allowed]) => {
+    const {globals, props} = noGlobals({
+      require: 'use import instead',
+      ...Object.fromEntries(['localStorage', 'sessionStorage'].map(k => [k, 'use storageManager instead'])),
+      XMLHttpRequest: 'use ajax.js instead'
+    })
+    return commonConfig({
+      files: sourcePattern(path),
+      plugins: {
+        prebid,
+      },
+      rules: {
+        'prebid/validate-imports': ['error', allowed],
+        'no-restricted-globals': [
+          'error',
+          ...globals
+        ],
+        'no-restricted-properties': [
+          'error',
+          ...props,
+          {
+            property: 'cookie',
+            object: 'document',
+            message: 'use storageManager instead'
+          },
+          {
+            property: 'sendBeacon',
+            object: 'navigator',
+            message: 'use ajax.js instead'
+          },
+          ...['outerText', 'innerText'].map(property => ({
+            property,
+            message: 'use .textContent instead'
+          })),
+          {
+            property: 'getBoundingClientRect',
+            message: 'use libraries/boundingClientRect instead'
+          },
+          ...['scrollTop', 'scrollLeft', 'innerHeight', 'innerWidth', 'visualViewport'].map((property) => ({
+            object: 'window',
+            property,
+            message: 'use utils/getWinDimensions instead'
+          }))
+        ]
+      }
+    })
+  }),
+  {
+    files: ['**/*BidAdapter.js'],
+    rules: {
+      'no-restricted-imports': [
+        'error', {
+          patterns: [
+            '**/src/events.js',
+            '**/src/adloader.js'
+          ]
+        }
+      ]
+    }
+  },
+  commonConfig({
+    files: sourcePattern('test'),
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+        ...globals.chai,
+        'sinon': false
+      }
+    },
+    rules: {
+      // tests were not subject to many rules and they are now a nightmare
+      'no-template-curly-in-string': 'off',
+      'no-unused-expressions': 'off',
+      'one-var': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'import/extensions': 'off',
+      'camelcase': 'off',
+      'no-array-constructor': 'off',
+      'import-x/no-duplicates': 'off',
+      'import-x/no-absolute-path': 'off',
+      'no-loss-of-precision': 'off',
+      'no-redeclare': 'off',
+      'no-global-assign': 'off',
+      'default-case-last': 'off',
+      '@stylistic/no-mixed-spaces-and-tabs': 'off',
+      '@stylistic/no-tabs': 'off',
+      '@stylistic/no-trailing-spaces': 'off'
+    }
+  })
+]
